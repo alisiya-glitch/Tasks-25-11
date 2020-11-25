9746590b914a54f30a14485fdd0cc4ec1ad84dd4
alisiya-glitch <75012885+alisiya-glitch@users.noreply.github.com>
2020-11-25 13:53:01 +0300

    Add files via upload

---
+++ second.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+a1 = a // 1000
+b1 = a // 100 % 10
+while (((a1 * 10 + b1) * 10 + b1) * 10 + a1) <= b:
+    print(a1, b1, b1, a1, sep="")
+    b1 = b1 + 1
+    if ( b1 > 9):
+        a1 = a1 + 1
+        b1 = 0
